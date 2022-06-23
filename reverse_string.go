c7bb62d1e10f3b6ff7a08cba309c332898943feb
IlyaGrushevsky <96526822+IlyaGrushevsky@users.noreply.github.com>
2022-06-23 13:17:05 +0300

    Add files via upload

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	r := []rune(input)
+	var src []rune
+	for i := len(r) - 1; i >= 0; i-- {
+		src = append(src, r[i])
+	}
+	output = string(src)
 	return output
 }
